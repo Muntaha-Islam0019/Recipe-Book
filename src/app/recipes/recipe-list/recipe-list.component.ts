1195e3650d810f9991f65a7ddb540dd37a0ff0d0
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-05-30 10:13:32 +0600

    Injected recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,22 +10,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Zoodle',
-      'This is made with Zucchini',
-      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759501/zucchini-kb-main-210810.jpg'
-    ),
-    new Recipe(
-      'Bean & Beef Chili',
-      'This is made with Bean and Beef',
-      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2020_39/1611689/fall-recipes-te-main-200921.jpg'
-    ),
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Zoodle',
+      'This is made with Zucchini',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759501/zucchini-kb-main-210810.jpg'
+    ),
+    new Recipe(
+      'Bean & Beef Chili',
+      'This is made with Bean and Beef',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2020_39/1611689/fall-recipes-te-main-200921.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
