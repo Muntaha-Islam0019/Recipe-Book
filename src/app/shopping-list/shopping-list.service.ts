98cfed3dfe2c1b9241206f0c2b8b4ef1b52d9605
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-05-30 12:14:32 +0600

    Injected the shopping-list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Beans', 100),
+    new Ingredient('Beef', 2),
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
